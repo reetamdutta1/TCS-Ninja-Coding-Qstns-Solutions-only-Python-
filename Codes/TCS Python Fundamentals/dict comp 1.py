b79086bb1292d6aedc3ebca0af69dc07341d6edc
Reetam Dutta <reetamdutta1@gmail.com>
2021-08-27 18:11:01 +0530

    Add files via upload

---
+++ Codes/TCS Python Fundamentals/dict comp 1.py
@@ -0,0 +1,3 @@
+names=["Rajesh", "Pritam", "Juhi", "Farha"]
+name_length={l:len(l) for l in names}
+print (name_length)
---
+++ Codes/TCS Python Fundamentals/dict comp 2.py
@@ -0,0 +1,3 @@
+Persons={'Pooja':18, 'Farhan':22, 'Shital':21, 'Rosy':19}
+seniors={key:value for (key, value) in Persons.items() if value>20}
+print (seniors)
